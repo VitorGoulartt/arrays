5124f8142ca9a3bfa780f553ec63334b24ac11c8
Vitor hugo <99295735+VitorGoulartt@users.noreply.github.com>
2025-10-24 01:00:59 +0000

    23/10

---
+++ Atividades/src/atividade1/Um.java
@@ -0,0 +1,26 @@
+package atividade1;
+import java.utill.Arrays;
+
+public class Um {
+
+    
+
+    public voi arrays(){
+        int[] acesso = {1, 2, 3, 4, 5};
+        System.out.println(acesso[1]);
+
+    }
+
+
+   
+
+    
+
+
+
+
+
+
+
+
+}
